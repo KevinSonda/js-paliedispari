9db44400e7d83785a1768f3550360bb993d6ec04
KevinSonda <kevinsonda@gmail.com>
2024-07-10 17:38:28 +0200

    [UPD]Dischiaro la variabile che conterrà la parola dell'utente e assegno prompt

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // definizione della funzine
  function checkPalindroma(string) {
     // definisco la variabile che dovra contenere la parola al contrario
-    let check_palindroma;
+    let check_palindroma = '';
 
     // prendo la parola passata come parametro e la splitto la inverto e riunisci i caratteri
     check_palindroma = string.split('').reverse().join('');
@@ -14,3 +14,6 @@
         console.log('La stringa non e palindroma')
     }
  }
+
+ // dischiaro la variabile che dovra contenere la parola dell'utente e gli assegno il prompt
+ let word = prompt('Inserisci la parola da verificare')
