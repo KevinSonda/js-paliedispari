d05f38658877a7ab007c8bc68112424dec849992
KevinSonda <kevinsonda@gmail.com>
2024-07-10 17:30:54 +0200

    [UPD] prendo la parola passata la splitto la inverto e riunisco i caratteri

--- js/script.js
+++ js/script.js
@@ -3,5 +3,6 @@
     // definisco la variabile che dovra contenere la parola al contrario
     let check_palindroma;
 
-    
+    // prendo la parola passata come parametro e la splitto la inverto e riunisci i caratteri
+    check_palindroma = string.split('').reverse().join('');
  }
