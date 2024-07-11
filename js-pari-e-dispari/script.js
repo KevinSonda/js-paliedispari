85eaa4db95a340bb4a73b45e6db84912ea5b9847
KevinSonda <kevinsonda@gmail.com>
2024-07-11 13:04:13 +0200

    [UPD]definisco la funzine che verifica se la somma e pari o dispari

--- js-pari-e-dispari/script.js
+++ js-pari-e-dispari/script.js
@@ -3,3 +3,13 @@ function generateRandomNumber() {
     return Math.floor(Math.random() * 5 + 1);
 }
 
+// definisco la funzione che verifica se la somma e pari o dispari 
+function checkEvenOrOdd(sum) {
+    // verifico se la somma e pari o dispari: se pari restituisco la stringa "pari" se dispari restituisco la stringa "dispari"
+    if (sum % 2 === 0) {
+        return 'pari';
+    }
+
+    return 'dispari';
+}
+
